858be983b21b9edd92e8786bc8501bbb46b14e52
Grzegorz Slowikowski <gslowikowski@gmail.com>
2013-11-29 07:37:47 +0000

    #4 - Possibility to add default Scala source roots ("src/main/scala" and "src/test/scala"). New "addScalaSources" mojo.
    git-svn-id: http://sbt-compiler-maven-plugin.googlecode.com/svn/trunk/plugin@37 d254d13a-b00a-acf6-21f5-a7ca4551998e

---
+++ src/main/java/com/google/code/sbt/SBTAddScalaSourcesMojo.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2013 Grzegorz Slowikowski (gslowikowski at gmail dot com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.google.code.sbt;
+
+import java.io.File;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.project.MavenProject;
+
+/**
+ * Add default Scala source roots, if they exist.
+ * 
+ * - adds src/main/scala to Maven project as compile source root
+ * - adds src/test/scala to Maven project as test compile source root
+ * 
+ * @author <a href="mailto:gslowikowski@gmail.com">Grzegorz Slowikowski</a>
+ * @since 1.0.0
+ */
+@Mojo( name = "addScalaSources", defaultPhase = LifecyclePhase.INITIALIZE )
+public class SBTAddScalaSourcesMojo
+    extends AbstractMojo
+{
+
+    /**
+     * <i>Maven Internal</i>: Project to interact with.
+     */
+    @Component
+    protected MavenProject project;
+
+    public void execute()
+        throws MojoExecutionException, MojoFailureException
+    {
+        File baseDir = project.getBasedir();
+
+        File mainScalaPath = new File( baseDir, "src/main/scala" );
+        if ( mainScalaPath.isDirectory() )
+        {
+            String mainScalaPathStr = mainScalaPath.getAbsolutePath();
+            if ( !project.getCompileSourceRoots().contains( mainScalaPathStr ) )
+            {
+                project.addCompileSourceRoot( mainScalaPathStr );
+                getLog().debug( "Added source directory: " + mainScalaPathStr );
+            }
+        }
+
+        File testScalaPath = new File( baseDir, "src/test/scala" );
+        if ( testScalaPath.isDirectory() )
+        {
+            String testScalaPathStr = testScalaPath.getAbsolutePath();
+            if ( !project.getCompileSourceRoots().contains( testScalaPathStr ) )
+            {
+                project.addTestCompileSourceRoot( testScalaPathStr );
+                getLog().debug( "Added test source directory: " + testScalaPathStr );
+            }
+        }
+    }
+
+}
