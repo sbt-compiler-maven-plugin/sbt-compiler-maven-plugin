671eb2fdecd58577b437a1e43c5001ec84f278f6
Grzegorz Slowikowski <gslowikowski@gmail.com>
2014-10-09 14:05:24 +0000

    #17 - Allow spaces in "javacOptions" and "scalacOptions" configuration parameters.
    git-svn-id: http://sbt-compiler-maven-plugin.googlecode.com/svn/trunk/plugin@167 d254d13a-b00a-acf6-21f5-a7ca4551998e

--- sbt-compiler-api/src/main/java/com/google/code/sbt/compiler/api/AbstractCompiler.java
+++ sbt-compiler-api/src/main/java/com/google/code/sbt/compiler/api/AbstractCompiler.java
@@ -20,6 +20,7 @@ package com.google.code.sbt.compiler.api;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.StringTokenizer;
 
 /**
  * An abstract base class for SBT compilers.
@@ -39,7 +40,7 @@ public abstract class AbstractCompiler
     {
         String scalacOptions = configuration.getScalacOptions();
         String sourceEncoding = configuration.getSourceEncoding();
-        List<String> result = new ArrayList<String>( Arrays.asList( scalacOptions.split( " " ) ) );
+        List<String> result = new ArrayList<String>( Arrays.asList( parseArgLine( scalacOptions ) ) );
         if ( !result.contains( "-encoding" ) && sourceEncoding != null && sourceEncoding.length() > 0 )
         {
             result.add( "-encoding" );
@@ -58,7 +59,7 @@ public abstract class AbstractCompiler
     {
         String javacOptions = configuration.getJavacOptions();
         String sourceEncoding = configuration.getSourceEncoding();
-        List<String> result = new ArrayList<String>( Arrays.asList( javacOptions.split( " " ) ) );
+        List<String> result = new ArrayList<String>( Arrays.asList( parseArgLine( javacOptions ) ) );
         if ( !result.contains( "-encoding" ) && sourceEncoding != null && sourceEncoding.length() > 0 )
         {
             result.add( "-encoding" );
@@ -67,4 +68,99 @@ public abstract class AbstractCompiler
         return result;
     }
 
+    // Copied from commons-exec org.apache.commons.exec.CommandLine.translateCommandline(String) method
+    // because it's private there and cannot be referenced from here.
+    /**
+     * Crack a command line.
+     *
+     * @param toProcess
+     *            the command line to process
+     * @return the command line broken into strings. An empty or null toProcess
+     *         parameter results in a zero sized array
+     */
+    private String[] parseArgLine( final String toProcess )
+    {
+        if ( toProcess == null || toProcess.length() == 0 )
+        {
+            // no command? no string
+            return new String[0];
+        }
+
+        // parse with a simple finite state machine
+
+        final int normal = 0;
+        final int inQuote = 1;
+        final int inDoubleQuote = 2;
+        int state = normal;
+        final StringTokenizer tok = new StringTokenizer( toProcess, "\"\' ", true );
+        final ArrayList<String> list = new ArrayList<String>();
+        StringBuilder current = new StringBuilder();
+        boolean lastTokenHasBeenQuoted = false;
+
+        while ( tok.hasMoreTokens() )
+        {
+            final String nextTok = tok.nextToken();
+            switch ( state )
+            {
+                case inQuote:
+                    if ( "\'".equals( nextTok ) )
+                    {
+                        lastTokenHasBeenQuoted = true;
+                        state = normal;
+                    }
+                    else
+                    {
+                        current.append( nextTok );
+                    }
+                    break;
+                case inDoubleQuote:
+                    if ( "\"".equals( nextTok ) )
+                    {
+                        lastTokenHasBeenQuoted = true;
+                        state = normal;
+                    }
+                    else
+                    {
+                        current.append( nextTok );
+                    }
+                    break;
+                default:
+                    if ( "\'".equals( nextTok ) )
+                    {
+                        state = inQuote;
+                    }
+                    else if ( "\"".equals( nextTok ) )
+                    {
+                        state = inDoubleQuote;
+                    }
+                    else if ( " ".equals( nextTok ) )
+                    {
+                        if ( lastTokenHasBeenQuoted || current.length() != 0 )
+                        {
+                            list.add( current.toString() );
+                            current = new StringBuilder();
+                        }
+                    }
+                    else
+                    {
+                        current.append( nextTok );
+                    }
+                    lastTokenHasBeenQuoted = false;
+                    break;
+            }
+        }
+
+        if ( lastTokenHasBeenQuoted || current.length() != 0 )
+        {
+            list.add( current.toString() );
+        }
+
+        if ( state == inQuote || state == inDoubleQuote )
+        {
+            throw new IllegalArgumentException( "Unbalanced quotes in " + toProcess );
+        }
+
+        final String[] args = new String[list.size()];
+        return list.toArray( args );
+    }
 }
