54671c1b76b553e3f62bc15375f0da1748ecb629
Grzegorz Slowikowski <gslowikowski@gmail.com>
2014-10-09 13:24:05 +0000

    Maven - small improvement:
        @Component
        protected MavenProject project;
    
    replaced with:
    
        @Parameter( defaultValue = "${project}", readonly = true, required = true )
        protected MavenProject project;
    
    as suggested on Maven mailing lists.
    
    git-svn-id: http://sbt-compiler-maven-plugin.googlecode.com/svn/trunk/plugin@165 d254d13a-b00a-acf6-21f5-a7ca4551998e

--- sbt-compiler-maven-plugin/src/main/java/com/google/code/sbt/compiler/plugin/SBTAddScalaSourcesMojo.java
+++ sbt-compiler-maven-plugin/src/main/java/com/google/code/sbt/compiler/plugin/SBTAddScalaSourcesMojo.java
@@ -20,9 +20,9 @@ package com.google.code.sbt.compiler.plugin;
 import java.io.File;
 
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
 /**
@@ -45,7 +45,7 @@ public class SBTAddScalaSourcesMojo
     /**
      * Maven project to interact with.
      */
-    @Component
+    @Parameter( defaultValue = "${project}", readonly = true, required = true )
     protected MavenProject project;
 
     /**
