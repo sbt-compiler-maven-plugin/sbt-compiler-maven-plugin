57ced886d4a56fc7d54e8a9a0cbe6044a8c05fb4
Grzegorz Slowikowski <gslowikowski@gmail.com>
2015-10-20 08:36:55 +0200

    Slightly improved javadoc comment.

--- sbt-compiler-maven-plugin/src/main/java/com/google/code/sbt/compiler/plugin/SBTAddManagedSourcesMojo.java
+++ sbt-compiler-maven-plugin/src/main/java/com/google/code/sbt/compiler/plugin/SBTAddManagedSourcesMojo.java
@@ -28,7 +28,7 @@ import org.apache.maven.project.MavenProject;
 /**
  * Add managed source root, if it is not already added.<br><br>
  * Helper mojo.<br>
- * Adds <code>${project.build.directory}/src_managed</code> is added to project's compile source roots
+ * Adds <code>${project.build.directory}/src_managed</code> as compile source root
  * even if it does not exist yet (it may be created later by source generators).
  * 
  * @author <a href="mailto:gslowikowski@gmail.com">Grzegorz Slowikowski</a>
